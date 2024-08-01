4e475f48e7fdb629ca3eb81e611131f30e56ad58
Abdul Moiz <moiza0799@gmail.com>
2024-08-01 11:41:17 -0500

    Process to retrieve values

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -12,14 +12,23 @@ export class ReactiveComponent implements OnInit {
 
   registrationFrom!: FormGroup;
 
+  constructor(private _formBuilder: FormBuilder) { }
   ngOnInit(): void {
-    this.registrationFrom = new FormGroup({
-      name: new FormControl,
-      username: new FormControl,
-      password: new FormControl,
-      passwordRepeat: new FormControl,
 
-    })
+    // this.registrationFrom = new FormGroup({
+    //   name: new FormControl("Hello"),
+    //   username: new FormControl,
+    //   password: new FormControl,
+    //   passwordRepeat: new FormControl,
+
+    // })
+
+    this.registrationFrom = this._formBuilder.group({
+      name: "Hello",
+      username: {},
+      password: {},
+      passwordRepeat: {}
+    });
   }
 
   register(form: FormGroup) {
